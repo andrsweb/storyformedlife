a332f3d84bc8244b50aa757a79fe03b6368a8c7e
Andrei Stezenko <astezenko@markupus.tech>
2024-07-28 20:13:29 +0300

    feat: add text under the title

--- wp-content/themes/geffen-sfl/parts/hero-text.php
+++ wp-content/themes/geffen-sfl/parts/hero-text.php
@@ -1,12 +1,23 @@
-<section class="hero <?=$args['class'] ?? ''?>">
-   <div class="background">
-      <img src="<?= get_the_post_thumbnail_url() ?>" alt="">
-   </div>
-   <div class="info">
-      <div class="content">
-         <div class="content-wrapper">
-            <h1><?php the_title() ?></h1>
-         </div>
-      </div>
-   </div>
+<?php
+
+/**
+ * Hero text part.
+ */
+
+$hero_text = get_field( 'text_under_the_title' );
+?>
+
+<section class="hero <?= $args['class'] ?? '' ?>">
+	<div class="background">
+		<img src="<?= get_the_post_thumbnail_url() ?>" alt="">
+	</div>
+	<div class="info">
+		<div class="content">
+			<div class="content-wrapper">
+				<h1><?php the_title() ?></h1>
+
+				<?php if( $hero_text ) echo $hero_text ?>
+			</div>
+		</div>
+	</div>
 </section>
